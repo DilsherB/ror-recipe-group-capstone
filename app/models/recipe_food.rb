8998449643a26876afbf2c3af8b49d3cb102d529
Dilsher Balouch <dilsherb@gmail.com>
2023-09-06 13:34:22 +0300

    some validation added in models

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,10 @@
 class RecipeFood < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
+  
+  after_initialize :set_default_quantity
+
+  def set_default_quantity
+    self.quantity ||= 1
+  end
 end
